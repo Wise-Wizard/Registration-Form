4e65dbb3c0b61da9a677b284d0367fc4d0c97721
Saransh Shankar <saransh.shankar@gmail.com>
2023-01-19 14:56:34 +0000

    Updated UI

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 
 function App() {
+  const [fname, setFname] = React.useState();
+  const [lname, setLname] = React.useState();
+
+  function changeFname(event) {
+    const firstName = event.target.value;
+    setFname(firstName);
+  }
+  function changeLname(event) {
+    const lastName = event.target.value;
+    setLname(lastName);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fname} {lname}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input name="fName" placeholder="First Name" onChange={changeFname} />
+        <input name="lName" placeholder="Last Name" onChange={changeLname} />
         <button>Submit</button>
       </form>
     </div>
